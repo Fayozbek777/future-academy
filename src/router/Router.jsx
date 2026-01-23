7a4d54eecd66668f359c6a90dc9ea21960c618f7
fayoz <fayoz@fayoz.fayoz>
2026-01-23 15:28:43 +0500

    auth-section-done

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home.jsx";
+import LoginPage from "../pages/Auth/LoginPage.jsx";
+const LOGIN_PATH = import.meta.env.VITE_LOGIN_PATH;
 
 const Router = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path={`/${LOGIN_PATH}`} element={<LoginPage />} />
       </Routes>
     </div>
   );
